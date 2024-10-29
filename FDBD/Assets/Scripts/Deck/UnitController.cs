b8df2d2dce84c8c71a50b1bb5766d3f1bfbf2fd2
chloe050412 <92531179+chloe050412@users.noreply.github.com>
2024-10-30 03:11:03 +0900

    [오수안] 맵에서 유닛 선택, 선택한 유닛 이동 구현

---
+++ FDBD/Assets/Scripts/Deck/UnitController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class UnitController : MonoBehaviour
+{
+    [SerializeField] private GameObject selectedUnit;
+
+    public void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (selectedUnit == null)
+            {
+                if (Physics.Raycast(ray, out hit, Mathf.Infinity) && hit.transform.CompareTag("Unit"))
+                {
+                    selectedUnit = hit.collider.gameObject;
+                }
+            }
+            else
+            {
+                if(Physics.Raycast(ray, out hit, Mathf.Infinity))
+                {
+                    selectedUnit.GetComponent<NavMeshAgent>().SetDestination(hit.point);
+                }
+            }
+        }
+    }
+}
